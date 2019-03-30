451d74284e45861519051fb8338482acfa1a63f8
Muzaffar Omer <muzaffar.omer@gmail.com>
2019-03-30 07:29:40 +0300

    Add output variables in vars.tf for created instances IPs

--- output.tf
+++ output.tf
@@ -3,3 +3,20 @@ output "backend_private_ip" {
   description = "Backend server public ip"
   value       = "${aws_instance.backend_server.private_ip}"
 }
+
+
+output "bastion_server_private_ip" {
+  value = "${module.bastion_server.private_ip}"
+}
+
+output "bastion_server_public_ip" {
+  value = "${module.bastion_server.public_ip}"
+}
+
+output "web_server_private_ip" {
+  value = "${module.web_server.private_ip}"
+}
+
+output "web_server_public_ip" {
+  value = "${module.web_server.public_ip}"
+}
